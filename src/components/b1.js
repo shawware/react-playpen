9948863e3dce2009db155120488c9c938993f28f
David Shaw <david.shaw@fleetcutter.com>
2017-04-18 20:44:36 +1000

    Remove unnecessary variable.

--- src/components/b1.js
+++ src/components/b1.js
@@ -5,17 +5,16 @@ class B1 extends Component {
 	constructor(props) {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
-		this.buttonName = props.name;
 	}
 
 	handleClick() {
-		console.log(this.buttonName + " clicked");
+		console.log(this.props.name + " clicked");
 	}
 
 	render() {
 		return(
 			<button onClick={this.handleClick}>
-				{this.buttonName}
+				{this.props.name}
 			</button>
 		);
 	}
